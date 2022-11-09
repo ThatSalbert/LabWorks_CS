c8452c922fd3ddfc1e9eb2728ab27664d9b1e6b9
Salbert <numeoriginaldemail@gmail.com>
2022-11-09 14:54:28 +0200

    Lab 2 added.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import Implementations.ClassicalCiphers.CaesarCipherA.CaesarCipherA;
 import Implementations.ClassicalCiphers.CaesarCipherB.CaesarCipherB;
 import Implementations.ClassicalCiphers.Vigenere.Vigenere;
 import Implementations.ClassicalCiphers.Playfair.Playfair;
+import Implementations.SymmetricCiphers.RC4.RC4;
+import Implementations.SymmetricCiphers.SDES.SDES;
 
 import java.math.BigInteger;
 import java.util.Random;
@@ -42,10 +44,35 @@ public class Main {
         System.out.println("\u001B[32m" + "Playfair Cipher Decrypted Message: " + "\u001B[0m" + decryptedMessagePlayfair);
 
         System.out.println();
+        RC4 CipherRC4 = new RC4("tornado");
+        String encryptedMessageRC4 = CipherRC4.processMessage("hello");
+        System.out.println("\u001B[32m" + "RC4 Cipher Encrypted Message: " + "\u001B[0m" + encryptedMessageRC4);
+        String decryptedMessageRC4 = CipherRC4.processMessage(encryptedMessageRC4);
+        System.out.println("\u001B[32m" + "RC4 Cipher Decrypted Message: " + "\u001B[0m" + decryptedMessageRC4);
+
+        System.out.println();
+        SDES sdes = new SDES("1000111010");
+        int[] encryptedMessageArray = sdes.encryptMessage("11100101");
+        System.out.print("\u001B[32m" + "SDES Cipher Encrypted Message: " + "\u001B[0m");
+        for(int i = 0; i < encryptedMessageArray.length; i++){
+            System.out.print(((char)encryptedMessageArray[i]) + " ");
+        }
+        System.out.println();
+        StringBuilder encryptedMessageSDES = new StringBuilder();
+        for(int i = 0; i < encryptedMessageArray.length; i++){
+            encryptedMessageSDES.append((char) encryptedMessageArray[i]);
+        }
+        int[] decryptedMessageArray = sdes.decryptMessage(encryptedMessageSDES.toString());
+        System.out.print("\u001B[32m" + "SDES Cipher Decrypted Message: " + "\u001B[0m");
+        for(int i = 0; i < decryptedMessageArray.length; i++){
+            System.out.print(((char)decryptedMessageArray[i]) + " ");
+        }
+
+        System.out.println("\n");
         RSA RSAcipher = new RSA(BigInteger.probablePrime(512, new Random()), BigInteger.probablePrime(512, new Random()));
         String messageToEncryptRSA = "Some random text for RSA.";
         byte[] encryptedMessageRSA = RSAcipher.encryptMessage(messageToEncryptRSA.getBytes());
-        System.out.println("\u001B[32m" + "RSA Cipher Encrypted Message: " + "\u001B[0m" + RSA.bytesToString(encryptedMessageRSA));
+        System.out.println("\u001B[32m" + "RSA Cipher Encrypted Message: " + "\u001B[0m" + new String(encryptedMessageRSA));
         byte[] decryptedMessageRSA = RSAcipher.decryptMessage(encryptedMessageRSA);
         System.out.println("\u001B[32m" + "RSA Cipher Decrypted Message: " + "\u001B[0m" + new String(decryptedMessageRSA));
 
